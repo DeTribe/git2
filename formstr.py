72fc08eefa7f1fe749e2c5cdff2f93e3728bf267
DeTribe <Stepoverangstrum@gmail.com>
2026-02-02 02:25:05 -0800

    Outlier python assessment. Format String {}.format()

---
+++ formstr.py
@@ -0,0 +1,6 @@
+snack = "{} and {}".format("Burger", "Fries")
+print("\nRepleced: ", snack)
+snack = "{1} and {0}".format("Burger", "Fries")
+print("\nReplaced: ", snack)
+snack = "%s and %s" % ("Milk", "Cookies")
+print("\nSubstituted: ", snack)
