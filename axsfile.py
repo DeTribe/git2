be5298204fd139397de8c51936402d397355e93c
DeTribe <Stepoverangstrum@gmail.com>
2026-02-02 15:48:15 -0800

    Outlier py Assessment, Accessing Files

---
+++ axsfile.py
@@ -0,0 +1,15 @@
+file = open("example.txt", "w")
+print("File name: ", file.name)
+print("File Open Mode: ", file.mode)
+print("Readable: ", file.readable())
+print("Writeable: ", file.writable())
+
+def get_status(f):
+       if (f.closed != False):
+                return "Closed"
+       else:
+                return "Open"
+
+print("File Status", get_status(file))
+file.close()
+print("\nFile Status", get_status(file))
