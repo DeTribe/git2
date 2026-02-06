36de41d7630f23faed481ab7f340e11fa7b03924
DeTribe <Stepoverangstrum@gmail.com>
2026-02-05 17:11:26 -0800

    Outlier py Assessment. def set_values(self, width, height) def area(self): return rect=Rectangle() rect.set_values() tri_set_values()

---
+++ Triangle.py
@@ -0,0 +1,8 @@
+from Polygon import*
+
+class Triangle(Polygon):
+         def area(self):
+                  return ( self.width * self.height )/2 
+
+
+                 
---
+++ inherit.py
@@ -0,0 +1,11 @@
+from Rectangle import*
+from Triangle import*
+
+rect = Rectangle()
+tri  = Triangle()
+
+rect.set_values(4,5)
+tri.set_values(4,5)
+
+print("Rectangle Area: ", rect.area())
+print("Triangle Area: ", tri.area())
